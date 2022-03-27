6dd45856af72a93962b9edc7b700f2fb0693c9e8
Kyun Heo <kyun2dot@gmail.com>
2022-03-27 17:01:09 +0900

    feat: add swagger api docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
+@ApiTags('test')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'test용 api 입니다.' })
+  @ApiResponse({ status: 200, description: '성공' })
   getHello(): string {
     return this.appService.getHello();
   }
