17966775b742aa8ec85cb5980e709cd0957b951a
Kyun Heo <kyun2dot@gmail.com>
2022-04-03 01:05:43 +0900

    feat: add user connect log

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -26,11 +26,15 @@ export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
   private logger: Logger = new Logger(EventsGateway.name);
 
   afterInit(nsp: Namespace) {
-    this.logger.log(`WS server init: ${nsp?.name}`);
+    this.logger.log(`WS 서버가 초기화되었습니다: ${nsp?.name}`);
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`유저가 접속했습니다: ${client.id}`);
   }
 
   handleDisconnect(client: Socket) {
-    this.logger.log(`IOClient disconnected: ${client.id}`);
+    this.logger.log(`유저가 접속을 끊었습니다: ${client.id}`);
   }
 
   @SubscribeMessage('test')
@@ -46,7 +50,7 @@ export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
     },
   })
   test(@ConnectedSocket() client: Socket, @MessageBody() data: string) {
-    this.logger.log(`data from client ${client.id} : ${JSON.stringify(data)}`);
+    this.logger.log(`유저 아이디 ${client.id} : ${JSON.stringify(data)}`);
     this.server.emit('test', { id: client.id, message: data });
   }
 
