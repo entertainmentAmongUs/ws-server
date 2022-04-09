efd1eb1a156c78a75d713719dc7e6cf1e26926de
Kyun Heo <kyun2dot@gmail.com>
2022-04-10 01:53:57 +0900

    test: remove event

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
-import { EventsModule } from './events/events.module';
 import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-  imports: [CoreModule, EventsModule, RoomsModule],
+  imports: [CoreModule, RoomsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,67 +1,52 @@
-import { Logger } from '@nestjs/common';
-import { ApiProperty } from '@nestjs/swagger';
-import {
-  ConnectedSocket,
-  MessageBody,
-  OnGatewayDisconnect,
-  OnGatewayInit,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-} from '@nestjs/websockets';
-import { AsyncApiPub, AsyncApiService, AsyncApiSub } from 'nestjs-asyncapi';
-import { Namespace, Server } from 'socket.io';
-import { Socket } from 'socket.io-client';
+// class TestEventsDto {
+//   @ApiProperty()
+//   readonly message: string;
+// }
 
-class TestEventsDto {
-  @ApiProperty()
-  readonly message: string;
-}
+// @AsyncApiService()
+// @WebSocketGateway({ cors: { origin: '*' }, allowEIO3: true })
+// export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
+//   @WebSocketServer()
+//   server: Server;
+//   private logger: Logger = new Logger(EventsGateway.name);
 
-@AsyncApiService()
-@WebSocketGateway({ cors: { origin: '*' }, allowEIO3: true })
-export class EventsGateway implements OnGatewayInit, OnGatewayDisconnect {
-  @WebSocketServer()
-  server: Server;
-  private logger: Logger = new Logger(EventsGateway.name);
+//   afterInit(nsp: Namespace) {
+//     this.logger.log(`WS 서버가 초기화되었습니다: ${nsp?.name}`);
+//   }
 
-  afterInit(nsp: Namespace) {
-    this.logger.log(`WS 서버가 초기화되었습니다: ${nsp?.name}`);
-  }
+//   handleConnection(client: Socket) {
+//     this.logger.log(`유저가 접속했습니다: ${client.id}`);
+//   }
 
-  handleConnection(client: Socket) {
-    this.logger.log(`유저가 접속했습니다: ${client.id}`);
-  }
+//   handleDisconnect(client: Socket) {
+//     this.logger.log(`유저가 접속을 끊었습니다: ${client.id}`);
+//   }
 
-  handleDisconnect(client: Socket) {
-    this.logger.log(`유저가 접속을 끊었습니다: ${client.id}`);
-  }
-
-  @SubscribeMessage('test')
-  @AsyncApiPub({
-    channel: 'test',
-    summary: '패킷 전송을 테스트합니다.',
-    description: '테스트할 때 서버는 해당 데이터를 보냅니다.',
-    message: {
-      name: 'test data',
-      payload: {
-        type: TestEventsDto,
-      },
-    },
-  })
-  @AsyncApiSub({
-    channel: 'test',
-    summary: '패킷 전송을 테스트합니다.',
-    description: '클라이언트는 테스트를 위해 해당 데이터를 보내야 합니다.',
-    message: {
-      name: 'test data',
-      payload: {
-        type: TestEventsDto,
-      },
-    },
-  })
-  test(@ConnectedSocket() client: Socket, @MessageBody() data: string) {
-    this.logger.log(`유저 아이디 ${client.id} : ${JSON.stringify(data)}`);
-    this.server.emit('test', { message: data });
-  }
-}
+//   @SubscribeMessage('test')
+//   @AsyncApiPub({
+//     channel: 'test',
+//     summary: '패킷 전송을 테스트합니다.',
+//     description: '테스트할 때 서버는 해당 데이터를 보냅니다.',
+//     message: {
+//       name: 'test data',
+//       payload: {
+//         type: TestEventsDto,
+//       },
+//     },
+//   })
+//   @AsyncApiSub({
+//     channel: 'test',
+//     summary: '패킷 전송을 테스트합니다.',
+//     description: '클라이언트는 테스트를 위해 해당 데이터를 보내야 합니다.',
+//     message: {
+//       name: 'test data',
+//       payload: {
+//         type: TestEventsDto,
+//       },
+//     },
+//   })
+//   test(@ConnectedSocket() client: Socket, @MessageBody() data: string) {
+//     this.logger.log(`유저 아이디 ${client.id} : ${JSON.stringify(data)}`);
+//     this.server.emit('test', { message: data });
+//   }
+// }
